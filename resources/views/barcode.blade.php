1b4681be2151cba5fe3d06403853f5deecb28b70
srikrishnarao <srikrishnarao@agent-network.com>
2020-01-27 13:32:32 +0900

    Initial commit

---
+++ resources/views/barcode.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<style type="text/css">
+		img{
+			padding-left: 20px;
+		}
+	</style>
+</head>
+<body>
+
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<h1 class="text-primary" style="text-align: center;">Laravel 5 Barcode Generator Using milon/barcode</h1>
+		</div>
+	</div>
+	
+	
+	<div class="container text-center" style="border: 1px solid #a1a1a1;padding: 15px;width: 70%;">
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('1A20912', 'C39')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('12', 'C39+')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('13', 'C39E')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('14', 'C39E+')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('15', 'C93')}}" alt="barcode" />
+		<br/>
+		<br/>
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('19', 'S25')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('20', 'S25+')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('21', 'I25')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('22', 'MSI+')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS1D::getBarcodePNG('23', 'POSTNET')}}" alt="barcode" />
+		<br/>
+		<br/>
+		<img src="data:image/png;base64,{{DNS2D::getBarcodePNG('16', 'QRCODE')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS2D::getBarcodePNG('17', 'PDF417')}}" alt="barcode" />
+		<img src="data:image/png;base64,{{DNS2D::getBarcodePNG('18', 'DATAMATRIX')}}" alt="barcode" />
+	</div>
+	
+	
+	
+</body>
+</html>
